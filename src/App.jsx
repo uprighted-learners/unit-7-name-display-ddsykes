54377318cdaaf2c80329c0f513983e14208ddb3d
ddsykes <ddsykes@hotmail.com>
2024-07-31 09:41:33 -0500

    Adding the Name display lab.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,30 @@
+
 import './App.css';
+import React, {useState } from 'react';
 
 function App() {
+  const [firstName, setFirstName] = useState('');
+  const [middleName, setMiddleName] = useState('');
+  const [lastName, setLastName] = useState('');
+  
+  const friendlyMessage =`Hello ${firstName} ${middleName} ${lastName} Welcome to the site`;
+
+
+  const handleFirstNameChange = (e) => setFirstName(e.target.value);
+  const handleMiddleNameChange = (e) => setMiddleName(e.target.value);
+  const handleLastNameChange = (e) => setLastName(e.target.value);
+
   return (
-    <>App</>
+    <>
+    <div class="app"></div>
+    <h1>{friendlyMessage}</h1>
+    <form>
+      <input type="text" placeholder="First Name" value={firstName} onChange={handleFirstNameChange}/>
+      <input type="text" placeholder='Middle Name' value={middleName} onChange={handleMiddleNameChange}/>
+      <input type="text" placeholder='Last Name' value={lastName} onChange={handleLastNameChange}/>
+    </form>
+    App
+    </>
   );
 }
 
